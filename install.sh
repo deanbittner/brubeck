967302e2bf41a68f5f056a2ee34ba39b9314c765
dean <dean@run-withit.com>
2019-02-05 18:36:51 +0000

    installer and update support

---
+++ install.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+
+TOTAL=5
+STEP=1
+
+# move the old out of the way
+#
+echo ""
+echo "$STEP/$TOTAL Stopping ..."
+STEP=$((STEP+1))
+if [ `which systemctl` ] ; then
+	systemctl stop brubeck > /dev/null 2>&1
+else
+	echo "No systemctl found.  Service not stopped"
+fi
+sleep 1
+[ -f /usr/local/sbin/brubeck ] && mv /usr/local/sbin/brubeck /usr/local/sbin/brubeck.old
+[ -f /etc/brubeck/config.json ] && mv /etc/brubeck/config.json /etc/brubeck/config.json.old
+[ -f /etc/init.d/brubeck ] && mv /etc/init.d/brubeck /etc/init.d/brubeck.old
+#
+# install the binary
+echo "$STEP/$TOTAL Installing binary ..."
+STEP=$((STEP+1))
+[ ! -d /usr/local/sbin ] && mkdir -p /usr/local/sbin
+cp brubeck /usr/local/sbin/brubeck
+chown root.root /usr/local/sbin/brubeck
+chmod +x /usr/local/sbin/brubeck
+#
+# install the init file
+echo "$STEP/$TOTAL Installing init script ..."
+STEP=$((STEP+1))
+if [ -d /etc/init.d ] ; then
+	cp brubeck_init.d /etc/init.d/brubeck
+	chown root.root /etc/init.d/brubeck
+	chmod +x /etc/init.d/brubeck
+	if [ `which systemctl` ] ; then
+	systemctl daemon-reload > /dev/null 2>&1
+	if [ `which chkconfig` ] ; then
+	    chkconfig --add brubeck
+	else
+	    if [ `which update-rc.d` ] ; then
+		update-rc.d brubeck defaults
+	    else
+		systemctl enable brubeck
+	    fi
+	fi
+	else
+	echo "No systemctl found.  Service not configured.  Init script installed."
+	fi
+fi
+
+echo "$STEP/$TOTAL Configuring ..."
+STEP=$((STEP+1))
+echo ""
+CONFIG_WORK="/tmp/config.json"
+rm -f "${CONFIG_WORK}"
+cat <<EOF >> "${CONFIG_WORK}"
+{
+    "sharding" : false,
+SERVER_NAME
+    "dumpfile" : "/var/log/brubeck.dump",
+    "capacity" : 2000,
+    "backends" :
+    [
+	{
+	    "type" : "carbon",
+CARBON_SERVER
+	    "port" : 2003,
+	    "frequency" : 10,
+	    "pickle" : false
+	}
+    ],
+    "samplers" :
+    [
+	{
+	    "type" : "statsd",
+	    "address" : "0.0.0.0",
+BRUBECK_PORT
+	    "workers" : 8,
+	    "multisock" : false,
+	    "multimsg" : 0
+	}
+    ]
+}
+EOF
+
+function set()
+{
+   MUNGE=$1
+   KEY=$2
+   VALUE=$3
+   FILE=$4
+
+   sed -i -e "s/${MUNGE}/            \"${KEY}\" : \"${VALUE}\"\,/g" $FILE
+}
+
+function getset()
+{
+   MUNGE=$1
+   KEY=$2
+   VALUE=$3
+   PROMPT=$4
+   FILE=$5
+
+   read -p "${PROMPT} [${VALUE}]: " RVALUE
+   [ ! -z "${RVALUE}" ] && VALUE="${RVALUE}"
+
+   set "${MUNGE}" "${KEY}" "${VALUE}" "${FILE}"
+}
+
+# get the vars
+# server_name
+HOSTNAME=`uname -n`
+getset "SERVER_NAME" "server_name" "brubeck-${HOSTNAME}" "Enter the server name, used for server metrics" "${CONFIG_WORK}"
+getset "CARBON_SERVER" "address" "bubble.bottorrent.net" "Enter a graphite/carbon server ip name or number" "${CONFIG_WORK}"
+getset "BRUBECK_PORT" "port" "8125" "Enter the listening port for brubeck, typically 8125." "${CONFIG_WORK}"
+
+read -p "Add datadog configuration [(y/n)n]: " RVALUE
+case $RVALUE in
+	Y|y)
+# the datadog blob
+DATADOG_CONFIG_WORK="/tmp/datadog.config.json"
+rm -f "${DATADOG_CONFIG_WORK}"
+cat <<EOF >> "${DATADOG_CONFIG_WORK}"
+	},
+	{
+	    "type" : "datadog",
+DATADOG_SERVER
+DATADOG_PORT
+	    "frequency" : 10,
+DATADOG_FILTER
+	    "expire" : 1
+EOF
+	getset "DATADOG_SERVER" "address" "localhost" "Enter a datadog server, typically localhost" "${DATADOG_CONFIG_WORK}"
+	getset "DATADOG_PORT" "port" "9125" "Enter the port of the datadog server, typically 9125 as we run brubeck on 8125" "${DATADOG_CONFIG_WORK}"
+	getset "DATADOG_FILTER" "filter" ".*" "Enter a regex.  Matches will be sent to datadog.  Defaults to all." "${DATADOG_CONFIG_WORK}"
+   	sed -i -e "/\"pickle\" : false/r${DATADOG_CONFIG_WORK}" "${CONFIG_WORK}"
+	;;	
+
+	*)
+	;;
+esac
+
+[ ! -d /etc/brubeck ] && mkdir -p /etc/brubeck
+cp "${CONFIG_WORK}" /etc/brubeck/config.json
+chown -R root.root /etc/brubeck/config.json
+
+echo ""
+echo "$STEP/$TOTAL Restarting ..."
+STEP=$((STEP+1))
+if [ `which systemctl` ] ; then
+	systemctl restart brubeck
+else
+	echo "No systemctl found.  Service not restarted."
+fi
---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+HOST_SYSTEM=`uname -s`
+COMPONENT=brubeck
+pushd ..
+
+if [ "${1}" == "local" ] && [ -f /tmp/runtime.${HOST_SYSTEM}.tar.gz ] ; then
+    cp /tmp/${COMPONENT}_runtime.${HOST_SYSTEM}.tar.gz .
+else
+    rm -f ${COMPONENT}_runtime.${HOST_SYSTEM}.tar.gz
+    wget http://trouble.bottorrent.net/static/RWI/runtime/${COMPONENT}_runtime.${HOST_SYSTEM}.tar.gz
+fi
+
+tar xvzf ${COMPONENT}_runtime.${HOST_SYSTEM}.tar.gz
+popd
+
