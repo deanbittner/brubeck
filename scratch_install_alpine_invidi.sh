154c92e8cea315d3bd665e0c208aa99336770e2a
dean <dean@run-withit.com>
2019-03-15 20:26:01 +0000

    jFrog integration with username and password prompting

--- scratch_install_alpine_invidi.sh
+++ scratch_install_alpine_invidi.sh
@@ -3,8 +3,12 @@
 HOST_SYSTEM=`uname -s`
 COMPONENT=brubeck_runtime
 
+while [ -z $FROGNAME ] ; do
+    read -p "Please enter your jFrog username: " FROGNAME
+done
+
 rm -f ${COMPONENT}.${HOST_SYSTEM}.tar.gz
-wget http://jfrog.invidi.com:80/artifactory/rwi-files-local/latest/brubeck_runtime.Linux.tar.gz
+wget --user ${FROGNAME} --ask-pass http://jfrog.invidi.com:80/artifactory/rwi-files-local/latest/${COMPONENT}.${HOST_SYSTEM}.tar.gz
 tar xvzf ${COMPONENT}.${HOST_SYSTEM}.tar.gz
 pushd ${COMPONENT}
 
